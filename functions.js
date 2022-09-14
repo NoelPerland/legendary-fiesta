2f203b34731ba29ee59920e6bfef6e4d97e5aae3
Noel Perland <112847803+NoelPerland@users.noreply.github.com>
2022-09-14 15:36:18 +0200

    Add files via upload
    I am not sure what we are supposed to do but i think this is right :D

---
+++ functions.js
@@ -0,0 +1,29 @@
+const prompt = require('prompt-sync')()
+
+const q = prompt(" Do you smell? ")
+
+function n() {
+    console.log("Nasty")
+}
+
+
+function f() {
+    console.log("Freshaf")
+}
+
+
+if (q =="yes") {
+    n();
+}
+
+else if (q == "no") {
+
+f();
+}
+
+
+
+
+
+**git add .** //detta skapar en commit
+git commit - m 'deez nutz' //detta namnger sketan
